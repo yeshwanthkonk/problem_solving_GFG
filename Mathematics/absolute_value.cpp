34d326717e290da09a003a696eb89f9096052559
Yeshwanth Konka <yeshwanthkonka@yahoo.com>
2023-08-19 20:01:14 +0530

    added absolute value program cpp

---
+++ Mathematics/absolute_value.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    public:
+        int absolute(int I) {
+        // Your code goes here
+        if (I>=0)
+            return I;
+        else
+            return 0-I;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;    // number of testcases
+    cin >> T; // input number of testcases
+    while (T--) {
+        int I;
+        cin >> I;
+        Solution ob;
+        cout << ob.absolute(I) << endl; // print the output
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+/*
+Test Case:
+1 12
+2 2332 -334
+*/
